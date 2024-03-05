615cb7af8ddf9c4748e192fc779ebafffe1d90ad
MarznaHuzei <marinka777xx@gmail.com>
2024-03-05 12:25:07 +0100

    wot

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,15 +1,15 @@
-import { FiSearch } from 'react-icons/fi';
-import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
-import { useState } from 'react';
+import { FiSearch } from "react-icons/fi";
+import { BtnSearch, Select, SearchFormStyled } from "./SearchForm.styled";
+import { useState } from "react";
 
 export const SearchForm = ({ onCountrySelect }) => {
-  const [country, setCountry] = useState('');
+  const [country, setCountry] = useState("");
 
-  const onChange = evt => {
+  const onChange = (evt) => {
     setCountry(evt.target.value);
   };
 
-  const onSubmit = evt => {
+  const onSubmit = (evt) => {
     evt.preventDefault();
     if (country) {
       onCountrySelect(country);
@@ -40,3 +40,42 @@ export const SearchForm = ({ onCountrySelect }) => {
     </SearchFormStyled>
   );
 };
+
+// export const SearchForm = ({ onCountrySelect }) => {
+//   const [country, setCountry] = useState("");
+
+//   const onChange = (evt) => {
+//     setCountry(evt.target.value);
+//   };
+
+//   const onSubmit = (evt) => {
+//     evt.preventDefault();
+//     if (country) {
+//       onCountrySelect(country);
+//     }
+//   };
+
+//   return (
+//     <SearchFormStyled onSubmit={onSubmit}>
+//       <BtnSearch type="submit">
+//         <FiSearch size="16px" />
+//       </BtnSearch>
+//       <Select
+//         aria-label="select"
+//         name="region"
+//         defaultValue="default"
+//         onChange={onChange}
+//         required
+//       >
+//         <option disabled value="default">
+//           Select a region and press enter
+//         </option>
+//         <option value="africa">Africa</option>
+//         <option value="america">America</option>
+//         <option value="asia">Asia</option>
+//         <option value="europe">Europe</option>
+//         <option value="Oceania">Oceania</option>
+//       </Select>
+//     </SearchFormStyled>
+//   );
+// };
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -5,40 +5,54 @@ import {
   Heading,
   Loader,
   CountryList,
-} from 'components';
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { fetchByRegion } from 'service/country-service';
+} from "components";
 
 export const CountrySearch = () => {
-  const [search, setSearchParams] = useSearchParams();
-  const [countries, setCountries] = useState([]);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const region = search.get('region');
-    if (!region) return;
-
-    setLoading(true);
-    fetchByRegion(region)
-      .then(setCountries)
-      .catch(error => setError(error.message))
-      .finally(setLoading(false));
-  }, [search]);
-
-  const onCountrySelect = region => {
-    setSearchParams({ region });
+  const onCountrySelect = (region) => {
+    console.log(region);
   };
-
   return (
     <Section>
       <Container>
         <SearchForm onCountrySelect={onCountrySelect} />
-        <CountryList countries={countries} />
-        {loading && <Loader />}
-        {error && <Heading>{error}</Heading>}
       </Container>
     </Section>
   );
 };
+
+// import { useEffect, useState } from 'react';
+// import { useSearchParams } from 'react-router-dom';
+// import { fetchByRegion } from 'service/country-service';
+
+// export const CountrySearch = () => {
+//   const [search, setSearchParams] = useSearchParams();
+//   const [countries, setCountries] = useState([]);
+//   const [error, setError] = useState('');
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     const region = search.get('region');
+//     if (!region) return;
+
+//     setLoading(true);
+//     fetchByRegion(region)
+//       .then(setCountries)
+//       .catch(error => setError(error.message))
+//       .finally(setLoading(false));
+//   }, [search]);
+
+//   const onCountrySelect = region => {
+//     setSearchParams({ region });
+//   };
+
+//   return (
+//     <Section>
+//       <Container>
+//         <SearchForm onCountrySelect={onCountrySelect} />
+//         <CountryList countries={countries} />
+//         {loading && <Loader />}
+//         {error && <Heading>{error}</Heading>}
+//       </Container>
+//     </Section>
+//   );
+// };
